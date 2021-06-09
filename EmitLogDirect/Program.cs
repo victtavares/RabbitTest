00728b1c7f86575e712c9b912032e7fa93b27195
Victor Tavares <victor.tavares@cubos.io>
2021-06-09 20:38:02 -0300

    feat: adjust line spaces in EmitLogsDirect.

--- EmitLogDirect/Program.cs
+++ EmitLogDirect/Program.cs
@@ -16,10 +16,7 @@ namespace EmitLogDirect {
                 while (true) {
                     Console.WriteLine($"Digite o texto (0 para sair):");
                     
-                    
                     var content = Console.ReadLine()!.Split(" ");
-
-                   
                     var message = content.Length > 0 ? content[0]: "";
                     //  'info', 'warning', 'error'.
                     var severity = content.Length > 1 ? content[1] : "info";
